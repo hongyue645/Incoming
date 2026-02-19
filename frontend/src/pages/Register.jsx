d3ab36f8ab14ce33eba927eb0506811cc001b139
hongyue645 <hongyue.zhang@student.lut.fi>
2026-02-19 21:49:47 +0800

    register

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,7 +1,66 @@
 import React, { useState } from 'react';
+import {FaRegUser} from 'react-icons/fa'
+import {TfiEmail} from 'react-icons/tfi'
+import {RiLockPasswordFill} from 'react-icons/ri'
 function Register() {
+    const [formData, setFormData] = useState({
+        username: '',
+        email: '',
+        password: '',
+        confirmPassword: '',
+        checked: false
+    });
+
+    const { username, email, password, confirmPassword, checked } = formData;
+
+    const onChange = (e) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value });
+    }
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        // Handle form submission logic here
+    }
+
     return (
-        <div>Register</div>
+        <>
+        <div className='register-page'>
+            <div className='register-left'>
+                <h2>Create your account</h2>
+                <p className='welcome-message'>Unlock all Features!</p>
+                <form className='register-form' onSubmit={onSubmit}>
+                    <div className='input-group'>
+                        <FaRegUser className='icon'/>
+                        <input type="text" placeholder='Username' value={username} onChange={(e) => setFormData({...formData, username: e.target.value})} />
+                    </div>
+                    <div className='input-group'>
+                        <TfiEmail className='icon'/>
+                        <input type="email" placeholder='Email' value={email} onChange={(e) => setFormData({...formData, email: e.target.value})} />
+                    </div>
+                    <div className='input-group'>
+                        <RiLockPasswordFill className='icon'/>
+                        <input type="password" placeholder='Password' value={password} onChange={(e) => setFormData({...formData, password: e.target.value})} />
+                    </div>
+                    <div className='input-group'>
+                        <RiLockPasswordFill className='icon'/>
+                        <input type="password" placeholder='Confirm Password' value={confirmPassword} onChange={(e) => setFormData({...formData, confirmPassword: e.target.value})} />
+                    </div>
+
+                    <div className='checkbox'>
+                        <label htmlFor='terms'>
+                            <input type="checkbox" id='terms' />
+                            Accept<a href="/terms" className='terms-link'>Terms and Conditions</a>
+                        </label>
+                    </div>
+
+                    <button type='submit'>Register</button>
+                </form>
+            </div>
+
+            <div className='register-right'>
+            </div>
+        </div>
+        </>
     )
 }
 
